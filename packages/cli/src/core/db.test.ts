4b11e8832329067d8d98c077dd0b5fb8318f0208
Sumit1993 <sumitpatel.14may@gmail.com>
2026-07-12 10:43:04 +0000

    chore: add SPDX header to db.test.ts

--- packages/cli/src/core/db.test.ts
+++ packages/cli/src/core/db.test.ts
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2026 Sumit Patel
+
 import { readdirSync } from "node:fs";
 import { rm } from "node:fs/promises";
 import { tmpdir } from "node:os";
